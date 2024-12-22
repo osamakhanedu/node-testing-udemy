3e24fdb0373189ec4f49ebee43095cbe2a2baa84
osamakhanedu <osamakhanedu@gmail.com>
2024-12-22 22:18:55 +0500

    feat: Add initial project setup with TypeScript, Jest, and utility function

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,7 @@
+import { toUpperCase } from "../app/Utils";
+describe('Utils test suite', () => {
+    test('should reuturn uppercase', () => {
+        const result = toUpperCase('abc');
+        expect(result).toBe('ABC');
+    })
+});
